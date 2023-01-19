df71370af1739d8b2f3d32598ed58a1f4c5ff927
Le Bao Hiep <baohiep2013@gmail.com>
2023-01-19 12:54:48 +0700

    executors: allow clock_nanosleep in Python

--- dmoj/executors/python_executor.py
+++ dmoj/executors/python_executor.py
@@ -29,6 +29,7 @@ import runpy, sys
 del sys.argv[0]
 runpy.run_path(sys.argv[0], run_name='__main__')
 """
+    syscalls = ['clock_nanosleep']
     address_grace = 131072
     data_grace = 2048
     ext = 'py'
